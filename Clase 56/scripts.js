7ad03b575ee23f497d58842b2078f4f456f28b04
carlafaes <carlafaes18@hotmail.com>
2022-06-11 18:58:49 -0300

    txt

--- Clase 56/scripts.js
+++ Clase 56/scripts.js
@@ -1,5 +1,9 @@
 //Especificación oficial: https://developer.mozilla.org/es/docs/IndexedDB-840092-dup
 
+// IndexedDB es una manera de almacenar datos dentro del navegador del usuario. Debido a que permite la creación de aplicaciones con habilidades de consulta enriquecidas, con independencia de la disponibilidad de la red, sus aplicaciones pueden trabajar tanto en línea como fuera de línea.
+
+
+
 const indexedDB = window.indexedDB
 const form = document.getElementById('form')
 
@@ -25,7 +29,7 @@ if (indexedDB && form) {
     }
 
     const addData = (data) => {
-        const transaction = db.transaction(['tasks'], 'readwrite')
+        const transaction = db.transaction(['tasks'], 'readwrite')//puede ser readonly o readwrite
         const objectStore = transaction.objectStore('tasks')
         const request = objectStore.add(data)
     }
